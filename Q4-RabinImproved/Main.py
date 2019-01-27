2be1cadfa25e42b2cf8e6b5cb9cb90c8ea724c25
JafarAkhondali <jafar.akhondali@yahoo.com>
2019-01-28 03:10:05 +0330

    + Fix typo :|

--- Q4-RabinImproved/Main.py
+++ Q4-RabinImproved/Main.py
@@ -23,7 +23,7 @@ def generate_key(key_file, count_of_bits):
 
     primes_list = []
     while len(primes_list) < 2:
-        rand_32_int = random.randrange(MIN_RANGE_NUMBER, MAX_RANGE_NUMBER ) | 11  # We only want numbers % 4 == 3, So we'll modify their lower bits to 3
+        rand_32_int = random.randrange(MIN_RANGE_NUMBER, MAX_RANGE_NUMBER ) | 3  # We only want numbers % 4 == 3, So we'll modify their lower bits to 3
         if rand_32_int not in primes_list and is_prime(rand_32_int):
             primes_list.append(rand_32_int)
     l("Chosen primes: " + str(primes_list))
